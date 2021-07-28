fcbbe8660d60613e0de5fb18854056010d11343a
OlehKharchenko <87533905+OlehKharchenko@users.noreply.github.com>
2021-07-28 18:26:31 +0300

    Tests now handling exceptions

--- KataSourceLib.Test/UnitTest1.cs
+++ KataSourceLib.Test/UnitTest1.cs
@@ -47,5 +47,32 @@ namespace TestProject1
         {
             Assert.Equal(11, CalculatorLogics.Add("//;\n2;4;5"));
         }
+
+        [Fact]
+        public void OneNegativeException()
+        {
+            try
+            {
+                CalculatorLogics.Add("3,-3,4,6");
+            }
+            catch (Exception ex)
+            {
+                Assert.Equal("negatives not allowed ( -3 )", ex.Message);
+            }
+
+        }
+
+        [Fact]
+        public void MultipleNegativesException()
+        {
+            try
+            {
+                CalculatorLogics.Add("//;\n3;-3;4;-6;5;7;-8");
+            }
+            catch (Exception ex)
+            {
+                Assert.Equal("negatives not allowed ( -3 -6 -8 )", ex.Message);
+            }
+        }
     }
 }
