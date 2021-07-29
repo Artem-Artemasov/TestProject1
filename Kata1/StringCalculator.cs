51dac467c87364dfb6332fbd529fa37d028e8ee1
OlehKharchenko <87533905+OlehKharchenko@users.noreply.github.com>
2021-07-29 17:39:52 +0300

    made some fixes after review
    code review by Yevhen Duiun and Anton Zhukov

--- Kata1/StringCalculator.cs
+++ Kata1/StringCalculator.cs
@@ -1,99 +1,95 @@
 ﻿using System;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace Kata1
 {
     public class StringCalculator
     {
-        //private const string RegexPattern = "^(\\d*)((((,||(\\n))(\\d*))*)||())$";  // One or more numbers separated by comma or/and new line (excluding everything else)
-        public static int Add(string numbers)
+        public int Add(string numbers)
         {
-            int[] resultArr;
-            string[] delimiters;
-
-            int negativesCount = 0;
-            string negativesMessage = "";
-
-            int result = 0;
-
             if (string.IsNullOrEmpty(numbers))
+            {
                 return 0;
-
-            delimiters = GetDelimiters(numbers);
-            numbers = GetNumbers(numbers);
-            resultArr = numbers.Split(delimiters, StringSplitOptions.None)
-                                .Select(str =>
-                                int.Parse(str)).ToArray();
-
-            ExcludeLargeNumbers(ref resultArr);
-
-            //counting negatives (if multiple) and generating error message
-            foreach (int number in resultArr)
-            {
-                if (number < 0)
-                {
-                    negativesCount++;
-                    negativesMessage += " " + number.ToString();
-                }
             }
 
+            List<int> resultList = GetNumbers(numbers, GetDelimiters(numbers));
+            resultList = ExcludeLargeNumbers(resultList);
 
-            if (negativesCount == 0)
+            string negativesMessage = GetNegativesString(resultList);
+
+            if (!string.IsNullOrEmpty(negativesMessage))
             {
-                result = resultArr.Aggregate((x, y) => x + y);
-                return result;
-            }
-            else
                 throw new Exception($"negatives not allowed ({negativesMessage} )");
-        }
+            }
 
-        private static string[] GetDelimiters(string numbers)
+            return resultList.Sum();
+        }
+        private List<string> GetDelimiters(string numbers)
         {
-            string[] delimiters = Array.Empty<string>();
-            string prefix;
+            List<string> delimiters = new();
 
             if (numbers.StartsWith("//"))
             {
                 int valuesStartIndex = numbers.IndexOf('\n');
-                prefix = numbers[2..valuesStartIndex];
+                string prefix = numbers[2..valuesStartIndex];
 
                 if (prefix.StartsWith('[') && prefix.EndsWith(']'))
-                    delimiters = prefix.Split(new char[] { ']', '[' }, StringSplitOptions.RemoveEmptyEntries);
+                {
+                    delimiters = prefix[1..^1].Split("][", StringSplitOptions.RemoveEmptyEntries).ToList();
+                }
                 else
-                    delimiters = delimiters.Append(numbers[2..valuesStartIndex]).ToArray(); // Takes symbols after "//" as delimiter
+                {
+                    delimiters.Add(numbers[2..valuesStartIndex]);
+                }
             }
             else
             {
-                delimiters = new string[] { ",", "\n" };
+                delimiters = new List<string> { ",", "\n" };
             }
 
             return delimiters;
         }
-        private static string GetNumbers(string numbers)
+        private List<int> GetNumbers(string numbers, List<string> delimiters)
         {
-            string result;
+
+            string result = numbers;
+
             if (numbers.StartsWith("//"))
             {
                 int valuesStartIndex = numbers.IndexOf('\n') + 1;
                 result = numbers.Substring(valuesStartIndex);
             }
-            else
-            {
-                result = numbers;
-            }
 
-            return result;
+            List<int> resultArr = result.Split(delimiters.ToArray(), StringSplitOptions.None)
+                                .Select(str =>
+                                int.Parse(str)).ToList();
+
+            return resultArr;
         }
-        private static void ExcludeLargeNumbers(ref int[] resultArr)
+        private List<int> ExcludeLargeNumbers(List<int> resultArr)
         {
-            int[] tempArr = Array.Empty<int>();
-            for (var i = 0; i < resultArr.Length; i++)
+            return resultArr.Where(x => x < 1000).ToList();
+        }
+        private string GetNegativesString(List<int> resultArr)
+        {
+
+            return " " + string.Join(" ", resultArr.Where(x => x < 0));
+            /*
+            return resultArr.Where(x => x < 0)
+                            .Select(x => " " + x.ToString())
+                            .Aggregate((x, y) => x + y);
+            /*
+            string negativesMessage = "";
+            foreach (int number in resultArr)
             {
-                if (resultArr[i] < 1000)
-                    tempArr = tempArr.Append(resultArr[i]).ToArray();
+                if (number < 0)
+                {
+                    negativesMessage += " " + number.ToString();
+                }
             }
-            resultArr = tempArr;
+            return negativesMessage;*/
+
         }
     }
 }
--- Kata1_Test/UnitTest1.cs
+++ Kata1_Test/UnitTest1.cs
@@ -6,92 +6,84 @@ namespace TestProject1
 {
     public class UnitTest1
     {
+        private StringCalculator calculator = new StringCalculator();
         [Fact]
         public void Add_WhenEmptyString_ShouldReturnsZero()
         {
             //act
-            int result = StringCalculator.Add("");
+            int result = calculator.Add("");
+
             //assert
             Assert.Equal(0, result);
         }
 
         [Fact]
-        public void Add_WhenOneNumberSting_Passing()
+        public void Add_WhenOneNumberSting_ReturnsSum()
         {
             //act
-            int result = StringCalculator.Add("7");
+            int result = calculator.Add("7");
+
             //assert
             Assert.Equal(7, result);
         }
 
         [Fact]
-        public void Add_WhenTwoNumbersWithComma_Passing()
+        public void Add_WhenTwoNumbersWithComma_ReturnsSum()
         {
             //act
-            int result = StringCalculator.Add("2,8");
+            int result = calculator.Add("2,8");
+
             //assert
             Assert.Equal(10, result);
         }
 
         [Fact]
-        public void Add_WhenMultipleNumbersInString_Passing()
+        public void Add_WhenMultipleNumbersInString_ReturnsSum()
         {
             //act
-            int result = StringCalculator.Add("2,2,5,5,6");
+            int result = calculator.Add("2,2,5,5,6");
+
             //assert
             Assert.Equal(20, result);
         }
 
         [Fact]
-        public void Add_WhenNewLineAsSplitter_Passing()
+        public void Add_WhenNewLineAsSplitter_ReturnsSum()
         {
             //act
-            int result = StringCalculator.Add("2\n8");
+            int result = calculator.Add("2\n8");
+
             //assert
             Assert.Equal(10, result);
         }
 
         [Fact]
-        public void Add_WhenNewLineAndCommaMixedAsSplitters_Passing()
+        public void Add_WhenNewLineAndCommaMixedAsSplitters_ReturnsSum()
         {
             //act
-            int result = StringCalculator.Add("2\n2,5");
+            int result = calculator.Add("2\n2,5");
+
             //assert
             Assert.Equal(9, result);
         }
 
         [Fact]
-        public void Add_WithCustomDelimiterPrefix_Passing()
+        public void Add_WithCustomDelimiterPrefix_ReturnsSum()
         {
             //act
-            int result = StringCalculator.Add("//;\n2;4;5");
+            int result = calculator.Add("//;\n2;4;5");
+
             //assert
             Assert.Equal(11, result);
         }
 
         [Fact]
-        public void Add_WhenOneNegative_ExceptionMessageCheck()
+        public void Add_WhenNegativesInString_ExceptionMessageCheck()
         {
             try
             {
                 //act
-                StringCalculator.Add("3,-3,4,6");
-            }
-            catch (Exception ex)
-            {
-                //assert
-                Assert.Equal("negatives not allowed ( -3 )", ex.Message);
-            }
-
-        }
-
-        [Fact]
-        public void Add_WhenMultipleNegatives_ExceptionMessageCheck()
-        {
-            try
-            {
-                //act
-                StringCalculator.Add("3,-3,4,-6,5,7,-8");
+                calculator.Add("3,-3,4,-6,5,7,-8");
             }
             catch (Exception ex)
             {
@@ -101,19 +93,21 @@ namespace TestProject1
         }
 
         [Fact]
-        public void Add_WhenNumberMoreThan1000_IgnoringThatNumber()
+        public void Add_WhenNumberMoreThan1000_IgnoresLargeNumbers()
         {
             //act
-            int result = StringCalculator.Add("3,4,5");
+            int result = calculator.Add("3,4,5,1500");
+
             //assert
             Assert.Equal(12, result);
         }
         
         [Fact]
-        public void Add_WhenDelimiterIsString_Passing()
+        public void Add_WhenDelimiterIsString_ReturnsSum()
         {
             //act
-            int result = StringCalculator.Add("//[***]\n1***2***3");
+            int result = calculator.Add("//[***]\n1***2***3");
+
             //assert
             Assert.Equal(6, result);
         }
@@ -122,7 +116,8 @@ namespace TestProject1
         public void Add_ForMultipleDelimiterStrings_Passing()
         {
             //act
-            int result = StringCalculator.Add("//[*a*][%]\n1*a*2%3*a*4%20");
+            int result = calculator.Add("//[*a*][%][[]\n1*a*2%3*a*4[20");
+
             //assert
             Assert.Equal(30, result);
         }
