830ef8dd9b67d2e0b19228dfe5d8fdd385a000e4
OlehKharchenko <87533905+OlehKharchenko@users.noreply.github.com>
2021-07-29 18:55:07 +0300

    Added console app

---
+++ TestProject.Kata1.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Kata1;
+
+namespace TestProject.Kata1.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+            string input;
+            StringCalculator calculator = new StringCalculator();
+
+            Console.Write("Enter comma separated numbers (enter to exit):\n\t> ");
+
+            while(running)
+            {
+                input = Console.ReadLine();
+                if(string.IsNullOrEmpty(input))
+                {
+                    running = false;
+                    continue;
+                }
+                try
+                {
+                    Console.WriteLine($"Result is: {calculator.Add(input)}");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"\n{ex.Message}\n\n");
+                }
+                Console.Write("Enter comma separated numbers (enter to exit):\n\t> ");
+            }
+        }
+    }
+}
